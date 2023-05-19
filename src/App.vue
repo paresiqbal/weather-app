a4b442e38960ca185f610679f8571b18dd073367
paresiqbal <thecrowdel@gmail.com>
2023-05-19 15:31:35 +0700

    Another push

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
-import { RouterView } from "vue-router";
 import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
     <SiteNavigation />
-    <RouterView />
   </div>
 </template>
 
