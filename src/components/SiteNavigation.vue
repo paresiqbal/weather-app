466fb19045e54c0598c77bd6d8b3568648053d41
paresiqbal <thecrowdel@gmail.com>
2023-05-19 15:48:28 +0700

    Many Change and not work

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -5,7 +5,6 @@
     >
       <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3">
-          <i class="fa-solid fa-sun text-2xl"></i>
           <p class="text-2xl">The Local Weather</p>
         </div>
       </RouterLink>
